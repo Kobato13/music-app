0e3980f9ccf10b962948ed3e0bfac4136108944d
Kobato13 <johncarlomartinez13@gmail.com>
2021-11-22 02:42:31 +0800

    Added resource/crud to albums controller and created form, edit and new html file for albums and tracks

--- app/controllers/albums_controller.rb
+++ app/controllers/albums_controller.rb
@@ -1,3 +1,54 @@
 class AlbumsController < ApplicationController
+  
+  before_action :set_album, only: [:edit, :update, :destroy]
+
+  def index
+    @albums = Album.all
+  end
+
+  def new 
+    @album = Album.new
+  end
+
+  def create
+    @album = Album.new(album_params)
+
+    if @album.save
+      flash[:notice] = "An album data was successfully created"
+      redirect_to albums_path
+    else
+      flash[:error] = "There are some errors encountered"
+      render :new
+    end
+
+  end
+
+  def edit
+  end
+
+  def update
+    if @album.update(album_params)
+      redirect_to albums_path, notice: "An album data  was successfully updated"
+    else
+      flash[:error] = "There are some errors encountered"
+      render :edit
+    end
+  end
+
+  def destroy
+    @album.destroy
+
+    redirect_to albums_path, notice: "An album data was successfully destroyed"
+  end
+
+  private
+
+  def set_album
+    @album = Album.find(params[:id])
+  end
+
+  def album_params
+    params.require(:album).permit(:name, :released, :released_at, :cover_art_url, :kind, :length)
+  end
 
 end
